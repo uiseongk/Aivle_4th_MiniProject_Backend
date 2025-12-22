a376a02e9b5d8b751d812bd9e17a361c1b6ffc9d
uiseongk <khyuna5509@gmail.com>
2025-12-22 16:29:58 +0900

    ip allowed

--- src/main/java/com/example/aivle_4th_MiniProject_team19/WebConfig.java
+++ src/main/java/com/example/aivle_4th_MiniProject_team19/WebConfig.java
@@ -13,6 +13,7 @@ public class WebConfig implements WebMvcConfigurer {
                 .allowedOrigins("http://localhost:3000")                // 허용할 출처 (URL)
                 .allowedOrigins("http://localhost:5173")
                 .allowedOrigins("http://a086131-front-website.s3-website-ap-southeast-1.amazonaws.com")
+                .allowedOrigins("http://54.151.138.19/")
                 .allowedMethods("GET", "POST", "PUT","PATCH", "DELETE", "OPTIONS") // HTTP 메서드 허용
                 .allowedHeaders("*")                                    // 모든 헤더 허용
                 .allowCredentials(false);                                // 쿠키 인증 요청 허용
